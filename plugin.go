a88c00b133df988a122d30b82816a8672e5e3e9e
Amit Limaye <amit@githedgehog.com>
2023-12-05 04:02:56 -0800

    WIP

--- plugin.go
+++ plugin.go
@@ -59,7 +59,7 @@ func Handler4(req, resp *dhcpv4.DHCPv4) (*dhcpv4.DHCPv4, bool) {
 
 	// }
 	// Is there a learnt subnet for (VrfName,VlanName)
-	circuitID := dhcpv4.RelayAgentInfo().String()
+	circuitID := req.RelayAgentInfo().String()
 	if len(circuitID) > 0 {
 		fmt.Println(circuitID)
 	}
