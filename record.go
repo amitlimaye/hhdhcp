97fe714606403ca53eecf4f85fa09efaec2a4ec4
Amit Limaye <amit@githedgehog.com>
2023-12-07 04:13:19 -0800

    LOgs

--- record.go
+++ record.go
@@ -40,7 +40,7 @@ func (r *recordBackend) GetRange(meta map[string]string) (*rangeRecord, error) {
 	if val, ok := meta["vrfName"]; ok {
 		log.Infof("found vrfName %s %v", val, r.subnets["VrfDhcp"])
 		if val != "VrfDhcp" {
-			log.Infof("What is this %s", val, "VrfDhcp")
+			log.Infof("What is this %s %s", val, "VrfDhcp")
 		}
 		return r.subnets[val], nil
 	}
